16512f2d4b95d44ed2fec9b2ed4a2a209fcba129
kekuso <kekuso808@gmail.com>
2016-09-17 13:57:47 -1000

    step7_scope

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -3,4 +3,5 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('myController', ['$scope', function ($scope) {
   $scope.myFirstName = 'Adam';
+  $scope.myModel = "Ready Player One";
 }]);
