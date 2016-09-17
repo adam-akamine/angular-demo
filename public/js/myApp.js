e2e22871948ef89350b67fde9f8ba1766724e6e3
kekuso <kekuso808@gmail.com>
2016-09-17 12:43:48 -1000

    step6_controller

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,1 +1,6 @@
+// second argument to import dependencies
 var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', ['$scope', function ($scope) {
+  $scope.myFirstName = 'Adam';
+}]);
