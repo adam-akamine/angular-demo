ec5b5d15f2b91a0cf66ca8c37bc0314f3e17e731
kekuso <kekuso808@gmail.com>
2016-09-17 14:59:29 -1000

    step11_factory

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('myApp')
-  .controller("myController", ['$scope', 'mainCharacter', function($scope, mainCharacter) {
+  .controller("myController", ['$scope', 'mainCharacter', 'CharacterVersionFactory', function($scope, mainCharacter, characterVersionFactory) {
+    console.log(characterVersionFactory);
     $scope.myFirstName = "Adam";
     $scope.myModel = "Ready Player One";
     $scope.mainCharacter = mainCharacter;
+    $scope.characterVersion = characterVersionFactory;
   }]);
