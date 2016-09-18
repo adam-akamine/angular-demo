d9c4d7903132703114b56beed07a18829d423357
kekuso <kekuso808@gmail.com>
2016-09-17 18:10:32 -1000

    Implemented Providers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,12 +4,18 @@ angular.module('myApp')
     'mainCharacter',
     'CharacterVersionFactory',
     'BookService',
-    function($scope, mainCharacter, characterVersionFactory, bookService) {
-    console.log(bookService.getBooks());
-    console.log(bookService.getBook(1));
+    'Movies',
+    function($scope, mainCharacter, characterVersionFactory, BookService, Movies) {
+    console.log(BookService.getBooks());
+    console.log(BookService.getBook(1));
     $scope.myFirstName = "Adam";
     $scope.myModel = "Ready Player One";
     $scope.mainCharacter = mainCharacter;
     $scope.characterVersion = characterVersionFactory;
-    $scope.books = bookService.getBooks();
+    $scope.BookService = BookService;
+    $scope.movies = [];
+    Movies.getMovies()
+      .then((response) => {
+        $scope.movies = response.data;
+      });
   }]);
--- public/js/myApp.js
+++ public/js/myApp.js
@@ -1,5 +1,9 @@
 // second argument to import dependencies
 var myApp = angular.module('myApp', [])
+  .config(['MoviesProvider',
+    function (MoviesProvider) {
+      MoviesProvider.setEndpoint('/api/movies');
+    }])
   .run(['$rootScope', 'APP_VERSION', function ($rootScope, APP_VERSION) {
     $rootScope.APP_VERSION = APP_VERSION;
   }]);
